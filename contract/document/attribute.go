25e523806d003d4f1c5c872c23253277887afc57
av <av.avramenko@gmail.com>
2026-04-16 02:56:03 +0300

    +

--- contract/document/attribute.go
+++ contract/document/attribute.go
@@ -1,13 +1,9 @@
 package document
 
 type Attribute struct {
-	ID        string   `json:"id"`
-	Name      string   `json:"name"`
-	Type      string   `json:"type"`
-	StringVal string   `json:"string_val,omitempty"`
-	LongVal   *int64   `json:"long_val,omitempty"`
-	DoubleVal *float64 `json:"double_val,omitempty"`
-	BoolVal   *bool    `json:"bool_val,omitempty"`
-	RefID     string   `json:"ref_id,omitempty"`
-	RefName   string   `json:"ref_name,omitempty"`
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Value   string `json:"value"`
+	ValueID string `json:"value_id,omitempty"`
+	DictID  string `json:"dict_id,omitempty"`
 }
